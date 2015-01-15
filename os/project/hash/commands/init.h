0058ef0edfa76aa5af23a68c98d3d72add4dab3d
vtemian <vladtemian@gmail.com>
2015-01-15 15:26:02 +0200

    Implemented uniq

--- os/project/hash/commands/init.h
+++ os/project/hash/commands/init.h
@@ -8,6 +8,7 @@
 #include "zen_r.h"
 #include "cd_r.h"
 #include "nl_r.h"
+#include "uniq_r.h"
 
 int list_size=0;
 command_nt *commands[MAX_CUSTOM_COMMANDS];
@@ -32,6 +33,7 @@ command_nt **get_custom_commands(int *size){
     add_custom_command("zen", &zen_r);
     add_custom_command("cd", &cd_r);
     add_custom_command("nl", &nl_r);
+    add_custom_command("uniq", &uniq_r);
 
     *size = list_size;
     return commands;
